45f7718698440f4d9ebd0b29d5c99ac30ecd50c7
Panbarasan V E <77746062+Arasanpanbu@users.noreply.github.com>
2021-03-19 01:13:52 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
+        <meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Welcome | Banking System</title>
+        <title>Welcome | Banking System</title>
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/bootstrap.css">	
+        <link rel="stylesheet" href="css/bootstrap.css">	
 </head>
 
 <body>
